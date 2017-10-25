b651cdcc5549aef892207e637e03b30398fb66b7
Nathan Klundt <nathanryan001@gmail.com>
2017-10-25 15:54:14 -0600

    completed all tasks on destructuring.js

--- destructuring.js
+++ destructuring.js
@@ -12,6 +12,7 @@ var carDetails = {
 }
 
 // CODE HERE
+var {color, make, model, year} = carDetails;
 
 
 // ========================
@@ -21,6 +22,7 @@ var carDetails = {
 
 function greeting( obj ) {
   // CODE HERE
+  var {firstName, lastName, title} = obj;
   
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
 }
@@ -32,6 +34,10 @@ function greeting( obj ) {
 // Write a function called totalPopulation that will take in an object. That object will have 4 properties named utah, california, texas and arizona. The property values will be numbers. Use object destructuring to save the property values to new variables. Sum up the values and return the total number.
 
   // CODE HERE
+  function totalPopulation(obj){
+    var {utah, california, texas, arizona} = obj; 
+    return utah + california + texas + arizona;
+  }
 
 
 // ========================
@@ -40,6 +46,12 @@ function greeting( obj ) {
 // Write a function called ingredients that will take in an object. This object will have 3 properties named carb, fat, and protein. The property values will be strings. Use object destructuring to save the property values to new variables. Push these new variables to an array and return the array. 
 
   // CODE HERE
+  function ingredients(obj) {
+    var {carb, fat, protein} = obj;
+    var arr = [];
+    arr.push(carb, fat, protein);
+    return arr;
+  }
 
 
 // ========================
@@ -54,6 +66,13 @@ function greeting( obj ) {
 // Write a function called largeNumbers that will take a destructured object as it's parameter. The object properties will be named first, second, and third and their values will be numbers. Find the smallest number of the three and return that number.
 
   // CODE HERE
+  function largeNumbers({first, second, third}){
+    var arr = [];
+    arr.push(first, second, third);
+    var low = first;
+    arr.forEach(  e => e < low ? low = e : low = low );
+    return low;
+  }
 
 
 // ========================
@@ -62,3 +81,18 @@ function greeting( obj ) {
 // Write a function called numberGroups that will take a destructured object as it's parameter. The object properties will be named a, b, and c and their values will be arrays of numbers. Find the longest array and return that array. 
 
   // CODE HERE
+var arrObj = {
+  a: [1,2,3,4,5,6,7,1,2,3],
+  b: [1,2],
+  c: [1,2,3]
+}
+  function numberGroups({a, b, c}){
+    //find longest of three arrays
+    //return longest arr
+    var arrOfArrs = [];
+    arrOfArrs.push(a,b,c);
+    var longest = arrOfArrs[0];
+    arrOfArrs.forEach( e => e.length > longest.length ? longest = e : Function.prototype );
+    return longest;
+  }
+console.log(numberGroups(arrObj));
